1309fb66317fab0c6d7d33543fa259d8c24f3c74
Jeongeun <110384223+jeongeuniii@users.noreply.github.com>
2022-11-25 13:28:18 +0900

    Update moodled.ino

--- moodlamp/moodled.ino
+++ moodlamp/moodled.ino
@@ -1,42 +1,23 @@
 #include <Adafruit_NeoPixel.h>
+
 Adafruit_NeoPixel strip(8, 6, NEO_GRB + NEO_KHZ800);
+int Leddelay=100;
+int RGB[8][3]={{204, 51, 0},{255, 153, 0},{255, 204, 0},{0, 255, 0},{0, 102, 0},{0, 0, 255},{51, 0, 204},{102, 51, 204}};
 
 void setup() {
   strip.begin();
-  strip.show();
   strip.setBrightness(250);
 }
 
 void loop() {
-  strip.setPixelColor(0, strip.Color(204, 51, 0));
-  strip.show();
-  delay(100);
-  strip.setPixelColor(1, strip.Color(255, 153, 0));
-  strip.show();
-  delay(100);
-  strip.setPixelColor(2, strip.Color(255, 204, 0));
-  strip.show();
-  delay(100);
-  strip.setPixelColor(3, strip.Color(0, 255, 0));
-  strip.show();
-  delay(100);
-  strip.setPixelColor(4, strip.Color(0, 102, 0));
-  strip.show();
-  delay(100);
-  strip.setPixelColor(5, strip.Color(0, 0, 255));
-  strip.show();
-  delay(100);
-  strip.setPixelColor(6, strip.Color(51, 0, 204));
-  strip.show();
-  delay(100);
-  strip.setPixelColor(7, strip.Color(102, 51, 204));
-  strip.show();
-  delay(100);
-
-  for(int i = 0; i < 8;i=i+1)
-  {
+for(int j=0; j<8;j++) {
+  for(int i=0; i<8; i++) { 
+    strip.setPixelColor(i, pixels.Color(RGB[j][0], RGB[j][1], RGB[j][2]));
+    strip.show();
+    delay(Leddelay);
     strip.setPixelColor(i, strip.Color(0, 0, 0));
     strip.show();
-    delay(100);
+    delay(110);
   }
+} 
 }
